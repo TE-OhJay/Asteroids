6c077dc3f4c833fca0f25859272751fb857832a9
OhJay <ollejardbrink@gmail.com>
2025-04-10 13:20:11 +0200

    game-loop init.

--- main.py
+++ main.py
@@ -5,10 +5,33 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+    
+    #Game loop initiation
+    while True:
+        
+        #Check if loop is interrupted (ie. quitting the game)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        #fill game screen with black
+        screen.fill("black")
+        
+        #Display update
+        pygame.display.flip()
+
+
+
 
 if __name__ == "__main__":
     main()
